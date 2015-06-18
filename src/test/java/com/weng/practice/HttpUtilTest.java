becc9b6a6dcfe39d66abd197a4f76017163d671b
翁世进 <wsjthinking@gmail.com>
2015-06-18 20:00:22 +0800

    dev
    dev

---
+++ src/test/java/com/weng/practice/HttpUtilTest.java
@@ -0,0 +1,12 @@
+package com.weng.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.weng.util.HttpUtil;
+
+public class HttpUtilTest {
+	public static void main(String[] args) {
+	
+	}
+}
