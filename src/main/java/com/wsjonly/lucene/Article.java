22f9cc87ff318eee5036ad2d1d2a4a52e97e8b18
翁世进 <wsjthinking@gmail.com>
2015-07-08 16:31:26 +0800

    lucene
    lucene learning

---
+++ src/main/java/com/wsjonly/lucene/Article.java
@@ -0,0 +1,42 @@
+package com.wsjonly.lucene;
+
+public class Article {
+
+	private Integer id;
+	private String title;
+	private String content;
+	
+	public Article() {
+		super();
+	}
+	public Article(Integer id, String title, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+	public synchronized Integer getId() {
+		return id;
+	}
+	public synchronized void setId(Integer id) {
+		this.id = id;
+	}
+	public synchronized String getTitle() {
+		return title;
+	}
+	public synchronized void setTitle(String title) {
+		this.title = title;
+	}
+	public synchronized String getContent() {
+		return content;
+	}
+	public synchronized void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", content=" + content + "]";
+	}
+	
+	
+}
