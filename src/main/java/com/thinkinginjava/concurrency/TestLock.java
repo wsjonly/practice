38b17cbcb90f3760d7b6f357086b1005b2386939
shijinweng <wsjthinking@gmail.com>
2015-08-03 23:35:21 +0800

    dev

---
+++ src/main/java/com/thinkinginjava/concurrency/TestLock.java
@@ -0,0 +1,27 @@
+package com.thinkinginjava.concurrency;
+
+import java.util.HashMap;
+
+public class TestLock {
+
+	private HashMap map = new HashMap();
+	
+	public TestLock() {
+		for(int i=0;i<100;i++) {
+			Thread t = new Thread(String.valueOf(i)) {
+				public void run() {
+					for (int j = 0; j < 500000; j++) {
+						map.put(new Integer(j), j);
+					}
+	
+					System.out.println("t" + this.getName() + " over");
+				}
+			};
+			t.start();
+		}
+	}
+
+	public static void main(String[] args) {
+		new TestLock();
+	}
+}
