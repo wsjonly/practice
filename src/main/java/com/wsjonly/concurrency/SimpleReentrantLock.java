97d474fc0f5852d0bd42705e8e121990934eec92
翁世进 <wsjthinking@gmail.com>
2015-08-07 22:04:23 +0800

    edit

---
+++ src/main/java/com/wsjonly/concurrency/SimpleReentrantLock.java
@@ -0,0 +1,14 @@
+package com.wsjonly.concurrency;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class SimpleReentrantLock extends AbstractQueuedSynchronizer {
+
+	private static final long serialVersionUID = -960613698093455583L;
+	
+	public SimpleReentrantLock() {
+		
+	}
+
+	
+}
